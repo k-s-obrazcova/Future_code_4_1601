03336a68f805724a0f600f9b3e523c5a8378189c
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-07-11 10:11:52 +0300

    11.07.2024

--- shop/tests/tests.py
+++ shop/tests/tests.py
@@ -6,20 +6,34 @@ from shop.utils import sum_price_count, CalculateMoney
 class CalculateMoneyDefTestCase(TestCase):
     def test_sum_count_price_pass(self):
         result = sum_price_count(price=100, count=10)
-        self.assertEquals(result, 1000)
+        self.assertEqual(result, 1000)
 
     def test_sum_count_price_2_pass(self):
         result = sum_price_count(500, 1)
-        self.assertEquals(result, 500)
+        self.assertEqual(result, 500)
 
     def test_sum_count_price_discount_pass(self):
         result = sum_price_count(price=200, count=15, discount=5)
-        self.assertEquals(result, 2850)
+        self.assertEqual(result, 2850)
 
 class CalculateMoneyClassTestCase(TestCase, CalculateMoney):
     def test_sum_count_price_pass(self):
         result = self.sum_price_count(price=100, count=10)
-        self.assertEquals(result, 1000)
+        self.assertEqual(result, 1000)
 
     def test_sum_price_pass(self):
         list_prices = [200, 640, 720]
+        result = self.sum_price(list_prices)
+        self.assertEqual(result, 1560)
+
+    def test_sum_price_with_discount(self):
+        list_prices = [200, 640, 720]
+        result = self.sum_price(list_prices, discount=10)
+        self.assertEqual(result, 1404)
+
+    def test_sum_price_more_element(self):
+        list_prices = [200,640, 720, 100, 40]
+        result = self.sum_price(list_prices)
+        self.assertEqual(result, 1700)
+
+
